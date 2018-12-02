616b1390c444726825b9fb427f742d059a9d5f10
afterburner <rockey1024@icloud.com>
2018-12-02 16:11:41 +0800

    Before clang-tidy fix

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #include <opencv2/imgcodecs.hpp>     // for imread, IMREAD_ANYCOLOR
 #include <opencv2/imgproc.hpp>       // for circle
 
+cv::Mat draft;
+
 
 int main(int argc, const char *argv[]) {
     constexpr const char *keys = "{help h usage ? |        | Please specify an input image}"
@@ -23,17 +25,29 @@ int main(int argc, const char *argv[]) {
         parser.printErrors();
     }
 
+// set mouse callback
     cv::String Lena{"Lena"};
     cv::namedWindow(Lena, cv::WINDOW_GUI_EXPANDED);
     const auto mouse_callback = [](const int event, const int x, const int y, const int flags, void *userdata) {
         if (event != cv::EVENT_LBUTTONDOWN) { return; }
         else {
-            cv::Mat &img_ = *static_cast<cv::Mat *>(userdata);
-            cv::circle(img_, cv::Point2i(x, y), 8, cv::Scalar(155, 100, 30), 2);
+            draft = *static_cast<cv::Mat *>(userdata);
+            cv::circle(draft, cv::Point2i(x, y), 8, cv::Scalar(155, 100, 30), 2);
         }
     };
     cv::setMouseCallback(Lena, mouse_callback, static_cast<void *>(&input_image));
+
+//    set trackbar
+//    int blurness = 0;
+    const auto on_change = [](const int pos, void *userdata){
+      if(!pos){
+          draft = *static_cast<cv::Mat *>(userdata);
+          cv::blur(draft, draft, cv::Size(pos, pos));
+      }
+    };
+    cv::createTrackbar("Blur size", Lena, nullptr, 11, on_change, static_cast<void *>(&input_image));
+
     while (cv::waitKey(30) != 27) {
-        cv::imshow(Lena, input_image);
+        cv::imshow(Lena, draft);
     }
 }
