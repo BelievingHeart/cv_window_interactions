e351464e9fe7c12befdb5956d7af21a604826c0b
afterburner <rockey1024@icloud.com>
2018-12-02 19:39:57 +0800

    Finish

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cv_window_interactions
+## This repo demos:
+1. Usage of CommandLineParser
+2. Usage of setMouseCallback
+3. Usage of createTrackbar
+4. Usage of createButton
+
+## Possible improvement:
+- multithreading
--- main.cpp
+++ main.cpp
@@ -1,17 +1,52 @@
 //
 // Created by afterburner on 18-12-2.
 //
-#include <opencv2/core/cvstd.hpp>    // for String
-#include <opencv2/core/mat.hpp>      // for Mat
-#include <opencv2/core/mat.inl.hpp>  // for Mat::Mat, Mat::~Mat, _InputArray...
-#include <opencv2/core/types.hpp>    // for Point2i, Scalar
-#include <opencv2/core/utility.hpp>  // for CommandLineParser
-#include <opencv2/highgui.hpp>       // for imshow, namedWindow, setMouseCal...
-#include <opencv2/imgcodecs.hpp>     // for imread, IMREAD_ANYCOLOR
-#include <opencv2/imgproc.hpp>       // for circle
+#include <jmorecfg.h>                    // for boolean
+#include <opencv2/core/cvstd.hpp>        // for String
+#include <opencv2/core/hal/interface.h>  // for CV_8U
+#include <opencv2/core/mat.hpp>          // for Mat
+#include <opencv2/core/mat.inl.hpp>      // for Mat::operator=, _InputArray:...
+#include <opencv2/core/types.hpp>        // for Point2i, Scalar, Size
+#include <opencv2/core/utility.hpp>      // for CommandLineParser
+#include <opencv2/highgui.hpp>           // for createButton, imshow, create...
+#include <opencv2/imgcodecs.hpp>         // for imread, IMREAD_ANYCOLOR
+#include <opencv2/imgproc.hpp>           // for Sobel, blur, circle, cvtColor
+#include <vector>                        // for vector
+
 
 cv::Mat draft;
+cv::Mat input_image;
+boolean to_color = 1;
+boolean to_gray = 0;
+boolean clear_points = 0;
+boolean sobel_on = 0;
+int blur_size = 0;
+std::vector<cv::Point2i> pts;
+cv::String Lena{"Lena"};
 
+
+//    overall change function
+void apply_changes(){
+    if(to_gray != 0){
+        cv::cvtColor(input_image, draft, cv::COLOR_BGR2GRAY);
+    }else{
+        draft = input_image.clone();
+    }
+    if(blur_size != 0){
+        cv::blur(draft, draft, cv::Size(blur_size, blur_size));
+    }
+    if(sobel_on != 0){
+        cv::Sobel(draft, draft, CV_8U, 1, 1);
+    }
+    if(clear_points != 0){
+        pts.clear();
+    }else{
+        for(auto& pt : pts){
+            cv::circle(draft, pt, 8, cv::Scalar(155, 100, 30), 2);
+        }
+    }
+    cv::imshow(Lena, draft);
+};
 
 int main(int argc, const char *argv[]) {
     constexpr const char *keys = "{help h usage ? |        | Please specify an input image}"
@@ -20,37 +55,58 @@ int main(int argc, const char *argv[]) {
     if (parser.has("help")) {
         parser.printMessage();
     }
-    cv::Mat input_image = cv::imread(parser.get<cv::String>("@image"), cv::IMREAD_ANYCOLOR);
+    input_image = cv::imread(parser.get<cv::String>("@image"), cv::IMREAD_ANYCOLOR);
     if (!parser.check()) {
         parser.printErrors();
     }
 
-// set mouse callback Black
-    cv::String Lena{"Lena"};
     cv::namedWindow(Lena, cv::WINDOW_GUI_EXPANDED);
+    cv::displayOverlay(Lena, "overlay 5 secs", 5000);
+
+// set mouse callback Black
     const auto mouse_callback = [](const int event, const int x, const int y, const int flags, void *userdata) {
         if (event != cv::EVENT_LBUTTONDOWN) { return; }
-
-        draft = (*static_cast<cv::Mat *>(userdata)).clone();
-        cv::circle(draft, cv::Point2i(x, y), 8, cv::Scalar(155, 100, 30), 2);
-
+        pts.emplace_back(x, y);
+        apply_changes();
     };
     cv::setMouseCallback(Lena, mouse_callback, static_cast<void *>(&input_image));
 
 //    set trackbar
-    int blurness = 0;
     const auto on_change = [](const int pos, void *userdata) {
         if (pos != 0) {
-//          draft = *static_cast<cv::Mat *>(userdata);
-            draft = (*static_cast<cv::Mat *>(userdata)).clone();
-
-            cv::blur(draft, draft, cv::Size(pos, pos));
+            blur_size = pos;
+            apply_changes();
         }
     };
-    cv::createTrackbar("Blur size", Lena, &blurness, 11, on_change, static_cast<void *>(&input_image));
+
+    int _ = 3;
+    cv::createTrackbar("Blur size", Lena, &_, 11, on_change, static_cast<void *>(&input_image));
+
+//    create buttons
+    const auto color_button = [](int state, void *userdata){
+        to_color = state;
+    };
+    cv::createButton("color", color_button, nullptr, cv::QT_RADIOBOX, true);
+
+    const auto gray_button = [](int state, void *userdata){
+        to_gray = state;
+        apply_changes();
+    };
+    cv::createButton("gray", gray_button, nullptr, cv::QT_RADIOBOX, false);
+
+   const auto sobel_button = [](int state, void *userdata){
+        sobel_on = state;
+        apply_changes();
+    };
+    cv::createButton("sobel", sobel_button, nullptr, cv::QT_CHECKBOX, false);
+
+    const auto clear_button = [](int state, void *userdata){
+        clear_points = state;
+        apply_changes();
+    };
+    cv::createButton("clear_points", clear_button, &clear_points, cv::QT_CHECKBOX, false);
 
     draft = input_image;
-    while (cv::waitKey(30) != 27) {
-        cv::imshow(Lena, draft);
-    }
+    cv::imshow(Lena, draft);
+    cv::waitKey(0);
 }
